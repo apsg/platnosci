68f3a65473fe45bea071fc23c6a193f8339bd2a7
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-04-28 17:51:48 +0200

    Fix string empty value on counter

--- app/Domains/Sales/Models/SalesObserver.php
+++ app/Domains/Sales/Models/SalesObserver.php
@@ -7,12 +7,25 @@ class SalesObserver
 {
     public function creating(Sale $sale)
     {
-        if (!empty($sale->hash)) {
-            return;
+        if (empty($sale->hash)) {
+            $sale->fill([
+                'hash' => Str::lower(Str::random(16)),
+            ]);
         }
 
-        $sale->fill([
-            'hash' => Str::lower(Str::random(16)),
-        ]);
+        if ($sale->counter === '') {
+            $sale->fill([
+                'counter' => null,
+            ]);
+        }
+    }
+
+    public function saving(Sale $sale)
+    {
+        if ($sale->counter === '') {
+            $sale->fill([
+                'counter' => null,
+            ]);
+        }
     }
 }
