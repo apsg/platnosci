4f64c7c25573220e296be62d05e771f9b2e3c578
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-10-04 12:08:40 +0200

    2022-10-04_12-08-40

--- app/Domains/Invoices/Invoice.php
+++ app/Domains/Invoices/Invoice.php
@@ -87,7 +87,6 @@ class Invoice
                 'total_price_gross' => $this->request->order->price,
                 'quantity'          => 1,
                 'quantity_unit'     => 'szt',
-                'lump_sum_tax'      => $this->provider->getLumpSum(),
             ]),
         ];
     }
