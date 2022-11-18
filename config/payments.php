5395ff8031b5380e343fe6c40cf3194076f25b1e
Gacek <szymon.gackowski@gmail.com>
2022-11-18 17:49:32 +0100

    added new p24 posid

--- config/payments.php
+++ config/payments.php
@@ -4,7 +4,7 @@ use App\Domains\Payments\PaymentsManager;
 use App\Domains\Payu\PayuHelper;
 
 return [
-    'payu' => [
+    'payu'    => [
         'driver'    => PaymentsManager::PAYU,
         'name'      => 'Payu testowy',
         'enabled'   => env('PAYU_POS_PLATNOSCI_ENABLED', true),
@@ -14,18 +14,25 @@ return [
         'secret'    => env('PAYU_SECRET'),
         'md5'       => env('PAYU_MD5'),
     ],
-    'p24'  => [
+    'p24'     => [
         'driver' => PaymentsManager::P24,
         'name'   => 'P24 BI',
         'posid'  => env('P24_POSID'),
         'token'  => env('P24_TOKEN'),
         'live'   => env('P24_LIVE', true),
     ],
-    'p24_pot'  => [
+    'p24_pot' => [
         'driver' => PaymentsManager::P24,
         'name'   => 'P24 OT',
         'posid'  => env('P24_OT_POSID'),
         'token'  => env('P24_OT_TOKEN'),
         'live'   => env('P24_OT_LIVE', true),
+    ],
+    'p24_imd' => [
+        'driver' => PaymentsManager::P24,
+        'name'   => 'P24 IMD',
+        'posid'  => env('P24_IMD_POSID'),
+        'token'  => env('P24_IMD_TOKEN'),
+        'live'   => env('P24_IMD_LIVE', true),
     ],
 ];
