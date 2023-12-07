575956aedd712609ca4bb1221325ee77aa685a90
gacek <szymon.gackowski@gmail.com>
2023-12-07 10:42:17 +0100

    2023-12-07_10-42-17

--- config/invoice.php
+++ config/invoice.php
@@ -48,9 +48,9 @@ return [
 //            'place'        => 'Paniówki',
             'url'           => env('FAKTUROWNIA_URL_IMD'),
             'token'         => env('FAKTUROWNIA_TOKEN_IMD'),
-//            'tax'          => 'zw',
-//            'lump_sum_tax' => '8,5',
-//            'description'  => 'Zwolnienie ze względu na nieprzekroczenie 200 000 PLN obrotu (art. 113 ust 1 i 9 ustawy o VAT)',
+            'tax'          => 'zw',
+            'lump_sum_tax' => '8,5',
+            'description'  => 'Zwolnienie ze względu na nieprzekroczenie 200 000 PLN obrotu (art. 113 ust 1 i 9 ustawy o VAT)',
 //            'bank_account' => '21 1050 1298 1000 0097 4030 0059',
         ],
         'eduinf'     => [
