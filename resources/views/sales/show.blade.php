ba9a2ad33e0b09244d1773ecaceb9e7e134f48ae
gacek <szymon.gackowski@gmail.com>
2024-02-10 18:39:08 +0100

    2024-02-10_18-39-08

--- resources/views/sales/show.blade.php
+++ resources/views/sales/show.blade.php
@@ -8,7 +8,7 @@
         <div class="min-h-[300px] flex-col justify-between">
             <div>
                 <div class="pb-12 border-b border-gray-200 mb-12">
-                    <img src="{{ \App\Images::logo() }}" class="mx-auto" />
+                    <img src="{{ \App\Images::logo() }}" class="mx-auto"/>
                 </div>
                 @if(!empty($sale->counter))
                     <div class="pb-3 md:pb-5 text-xl text-pink-600 font-bold">
@@ -16,16 +16,53 @@
                         <x-counter :counter="$sale->counter"></x-counter>
                     </div>
                 @endif
-                <h1 class="text-2xl font-bold mb-5">
+                @if(!empty($sale->icon_url))
+                    <img src="{{ $sale->icon_url }}" style="max-height: 140px "  class="mb-5"/>
+                @endif
+                @if(!empty($sale->title))
+                    <h1 class="text-2xl font-bold mb-5">
+                        {{ $sale->title }}
+                    </h1>
+                @endif
+                <p class="pb-5" style="color: #141311B2;">
                     {{ $sale->description }}
-                </h1>
+                </p>
                 @if($sale->full_price)
-                    <div class="text-red-700 text-3xl line-through decoration-2">
+                    <div class="line-through decoration-2 full-price font-semibold">
                         {{ $sale->format('full_price') }} PLN
                     </div>
                 @endif
-                <div class="text-5xl text-green-700">
+                <div class="price font-bold">
                     {{ $sale->format('price') }} PLN
+                </div>
+
+                <div class="border-t border-gray-200 mt-5 pt-5 ">
+                    <div class="grid grid-cols-12 gap-y-3">
+                        <div class="col-span-1 text-center content-center">
+                            <img class="mx-auto" src="{{ url('/images/icon_1.svg') }}"/>
+                        </div>
+                        <p class="descriptions col-span-11 content-center">
+                            Bezpieczne Płatności
+                        </p>
+                        <div class="col-span-1 text-center content-center">
+                            <img class="mx-auto" src="{{ url('/images/icon_2.svg') }}"/>
+                        </div>
+                        <p class="descriptions col-span-11 content-center">
+                            Natychmiastowy dostęp
+                        </p>
+                        <div class="col-span-1 text-center content-center">
+                            <img class="mx-auto" src="{{ url('/images/icon_3.svg') }}"/>
+                        </div>
+                        <p class="descriptions col-span-11 content-center">
+                            Transparentna Platforma: Maratony Excela, Zaufani trenerzy
+                        </p>
+                        <div class="col-span-1 text-center content-center">
+                            <img class="mx-auto" src="{{ url('/images/icon_4.svg') }}"/>
+                        </div>
+                        <p class="descriptions col-span-11 content-center">
+                            Najlepsza okazja + 30 dniowy zwrot
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
